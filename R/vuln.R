05277110181a1e44756872ea97003d9a9063e82b
phytomosaic <smithr2@oregonstate.edu>
2018-10-26 15:15:04 -0700

    Code format for vuln

--- R/vuln.R
+++ R/vuln.R
@@ -57,9 +57,8 @@
 #' @export
 #' @rdname vuln
 ###  general vulnerability function
-`vuln` <- function(spe, y, ybin, yinv, na.rm=TRUE, fullinfo=TRUE,
-                   ...){
-     y  <- f.as.matrix(y)               # force as matrix
+`vuln` <- function(spe, y, ybin, yinv, na.rm=TRUE, fullinfo=TRUE,...){
+     y   <- f.as.matrix(y)              # force as matrix
      tmp_abu <- tmp <- f.as.matrix(spe) # force as matrix
      tmp <- (tmp>0)*1                   # binary occurrences matrix
      tmp[tmp==0]         <- NA          # replace 0 with NA
@@ -69,17 +68,17 @@
      ncy <- ncol(y)                     # n explanatory variables
 
      ### compatibility checks
-     if(nr!=nrow(y)) stop('incompatible dims: `spe` and `y`')
-     if(!missing(ybin)){
+     if (nr!=nrow(y)) stop('incompatible dims: `spe` and `y`')
+     if (!missing(ybin)){
           if (!inherits(ybin, 'bingrid'))
                stop('`ybin` must be of class `bingrid`')
-          if(is.vector(ybin[[1]])) ybin <- list(ybin)
-          if(ncy!=length(ybin))
+          if (is.vector(ybin[[1]])) ybin <- list(ybin)
+          if (ncy!=length(ybin))
                stop('incompatible dims: `ybin`, `y`')
-          if(ncy!=1){
-               if(length(unique(lapply(ybin,dimnames)))!=1)
+          if (ncy!=1){
+               if (length(unique(lapply(ybin,dimnames)))!=1)
                     stop('incompatible dimnames in `ybin` list items')
-               if(!identical(dimnames(y)[[2]],names(ybin)))
+               if (!identical(dimnames(y)[[2]],names(ybin)))
                     stop('verify colnames identical for `ybin`, `y`')
           }
      }
@@ -92,11 +91,9 @@
      `ptile` <- function(j,a,b){
           a <- a[,j]
           b <- b[,j]
-          if( all(is.na(a)) |  all(is.na(b))  ){
-               message(
-                    paste0('no info for variable ', jj,
-                           ', species ',j))
-
+          if (all(is.na(a)) | all(is.na(b))){
+               message(paste0('no info for variable ', jj,
+                              ', species ',j))
                rep(NA, times=length(b))
           } else {
                ecdf(a)(b)
@@ -106,7 +103,7 @@
      ### for each explanatory variable, calculate ECDF, percentile
      for(jj in 1:ncy){
           v[,,jj] <- tmp * c(y[,jj])    # y vals at occupied sites
-          if(missing(ybin)){
+          if (missing(ybin)){
                vest <- v[,,jj]          # y vals for niche estimate
                message(paste0('ECDF from pointwise data: ',
                               nr, ' sites, ', nc ,
@@ -115,7 +112,7 @@
                vest <- f.as.matrix(ybin[[jj]]) # y vals for niche est
                nrb <- nrow(vest)
                ncb <- ncol(vest)
-               if(nc!=ncb) stop('incompatible dims: `spe` and `ybin`')
+               if (nc!=ncb) stop('incompatible dims: `spe` and `ybin`')
                message(paste0('ECDF from rasterized data: ',
                               nrb, ' sites, ', ncb,
                               ' species. Niche is extended.'))
@@ -125,8 +122,8 @@
      }
 
      ### invert by taking complement (if higher values more 'extreme')
-     if(!missing(yinv)){
-          if(!identical(length(yinv), ncy)){
+     if (!missing(yinv)){
+          if (!identical(length(yinv), ncy)){
                stop('length(yinv) != NCOL(y)')
           }
           r <- which(yinv)
@@ -134,7 +131,7 @@
      }
 
      ### calculate site average percentile across ALL y variables
-     if(ncy!=1){
+     if (ncy!=1){
           p <- rowMeans(pp, na.rm = !fullinfo, dims = 2)
      } else {
           p <- drop(pp)
@@ -152,7 +149,7 @@
           rowSums(vo,na.rm=na.rm)/rowSums(tmp,na.rm=na.rm))*100 # TVIa
      t2  <- rowMeans(p,na.rm=na.rm)*100 # TVIb, site avg of spp %tiles
      # calc vulnerability index C only if y is univariate
-     if(ncy==1){
+     if (ncy==1){
           p95 <- apply(vest, 2, quantile, probs=0.95, na.rm=na.rm)
           vc  <- sweep(tmp, 2, p95, `*`) # weighted UPPER tols (STIs)
           cwm <- rowMeans(vc, na.rm=na.rm) # CWM(CTI)=site avg STI
@@ -167,7 +164,7 @@
                  t2=t2,        # vulnerability indices
                  t3=NULL        # vulnerability indices
      )
-     if(exists('t3')) out$t3 <- t3
+     if (exists('t3')) out$t3 <- t3
      class(out) <- 'vuln'
      out
 }
@@ -183,17 +180,17 @@
 
 ### function always returns a matrix, even if passed a vector
 `f.as.matrix` <- function(x, ...){
-     if(is.vector(x) & is.numeric(x)){
+     if (is.vector(x) & is.numeric(x)){
           return(as.matrix(x, rownames.force=T, dimnames=dimnames(x)))
      }
-     if(!all(apply(x, 1, is.numeric)))
+     if (!all(apply(x, 1, is.numeric)))
           stop('data has non-numeric values')
      dn <- dimnames(x)
      dm <- dim(x)
      x <- as.matrix(x, rownames.force=TRUE, dimnames=dimnames(x))
-     if(!identical(dim(x),dm))
+     if (!identical(dim(x),dm))
           stop('unequal column lengths')
-     if(!identical(dimnames(x),dn))
+     if (!identical(dimnames(x),dn))
           stop('dimnames not retained')
      x
 }

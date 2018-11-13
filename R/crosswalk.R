cb1e717ba6c2e2198a2ed9f3f0b32bea3ad2bf3f
phytomosaic <smithr2@oregonstate.edu>
2018-11-13 11:10:40 -0800

    Add species names crosswalk

---
+++ R/crosswalk.R
@@ -0,0 +1,24 @@
+#' @name crosswalk
+#'
+#' @title Crosswalk between abbreviations and species names for North
+#'      American epiphytic macrolichens
+#'
+#' @docType data
+#'
+#' @description
+#' This data set gives information about FIA abbreviations and the
+#'      corresponding species names for North American macrolichens.
+#'
+#' @usage
+#' data(crosswalk)
+#'
+#' @details
+#' Species data were sourced from the US Forest Service's
+#'      Forest Inventory and Analysis program (FIA 2011).
+#'
+#' @references
+#' US Forest Service, Forest Inventory and Analysis [FIA]. 2011. Phase
+#'      3 Field Guide – Lichen Communities, Version 5.1.
+#'
+#' @keywords datasets
+"crosswalk"
